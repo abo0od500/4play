776350ce8f3bdd95819c59ec4595d04e85b5fc65
Abdullah <alkhamsi.abdullah@gmail.com>
2019-02-09 16:48:07 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
  
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-   client.user.setActivity("lplay",{type: 'WATCHING'})
+   client.user.setActivity("mplay",{type: 'WATCHING'})
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
@@ -40,7 +40,7 @@ const queue = new Map();
      
 
  
-var prefix = "l" 
+var prefix = "m" 
 client.on('message', async msg => {
     //--
 
